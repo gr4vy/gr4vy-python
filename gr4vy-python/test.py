e76d6290fc9052dde5782c7918f80a32fed39c26
Phillip Thomas <phillip@gr4vy.com>
2021-10-20 11:20:28 -0400

    Signed-off-by: Phillip Thomas <phillip@gr4vy.com>

--- gr4vy-python/test.py
+++
@@ -1,4 +0,0 @@
-from gr4vyClient import Gr4vyClient
-
-client = Gr4vyClient("spider","private_key.pem")
-client.listBuyers()
--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 import setuptools
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="gr4vy-python",
+from setuptools import setup,find_packages
+setup(
+    name="gr4vy_python",
     version="0.0.1",
-    packages=['gr4vy-python'],
+    packages=find_packages(),
     author="gr4vy",
     author_email="phillip@gr4vy.com",
     description="Gr4vy Python",
@@ -15,4 +12,10 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/gr4vy/gr4vy-python/issues",
     },
     python_requires=">=3.6",
-)
+    install_requires=["openapi-client==1.0.0",
+	"pem==21.2.0",
+	"PyJWT==2.3.0",
+	"python-dateutil==2.8.2",
+	"six==1.16.0",
+	"urllib3==1.26.7"]
+    )
